0a6f5e04b2106800c9a3d74d857034300424cd79
kishorkumar11 <h.kishorkumar7@gmail.com>
2023-02-23 18:46:51 +0800

    merge

--- Assets/Scripts/MQTT/mqttStateController.cs
+++ Assets/Scripts/MQTT/mqttStateController.cs
@@ -35,6 +35,8 @@ public class mqttStateController : MonoBehaviour
     private void OnMessageArrivedHandler(string newMsg)
     {
         var gameState = JsonUtility.FromJson<mqttState>(newMsg);
+        var p1 = gameState.p1;
+        var p2 = gameState.p2;
         
         if(gameState.correction == false) // No need correction. Show action and update all parameters
         {
@@ -44,35 +46,36 @@ public class mqttStateController : MonoBehaviour
                         
             // Correct player status and Update UI
     
-            float p1Health = Mathf.Clamp(gameState.p1.hp, 0, maxHealth);
-            float p1ShieldHealth = Mathf.Clamp(gameState.p1.shield_health, 0, maxShieldHealth);
+            float p1Health = Mathf.Clamp(p1.hp, 0, maxHealth);
+            float p1ShieldHealth = Mathf.Clamp(p1.shield_health, 0, maxShieldHealth);
             player1.UpdateHealth(p1Health);
-            player1.UpdateShieldHealth(gameState.p1.shield_health);
+            player1.UpdateShieldHealth(p1.shield_health);
 
-            float p2Health = Mathf.Clamp(gameState.p2.hp, 0, maxHealth);
-            float p2ShieldHealth = Mathf.Clamp(gameState.p2.shield_health, 0, maxShieldHealth);
-            player2.UpdateHealth(gameState.p2.hp);
-            player2.UpdateShieldHealth(gameState.p2.shield_health);
+            float p2Health = Mathf.Clamp(p2.hp, 0, maxHealth);
+            float p2ShieldHealth = Mathf.Clamp(p2.shield_health, 0, maxShieldHealth);
+            player2.UpdateHealth(p2.hp);
+            player2.UpdateShieldHealth(p2.shield_health);
 
             // Player 1
-            player1.UpdateBulletCount(gameState.p1.bullets);
-            player1.UpdateGrenadeCount(gameState.p1.grenades);
-            player1.UpdateShieldCount(gameState.p1.num_shield);
-            player1.deathCounter.UpdatePlayerDeathCount(gameState.p1.num_deaths);
-            
+            player1.UpdateBulletCount(p1.bullets);
+            player1.UpdateGrenadeCount(p1.grenades);
+            player1.UpdateShieldCount(p1.num_shield);
+            player1.deathCounter.UpdatePlayerDeathCount(p1.num_deaths);
+
             // Player 2   
-            player2.UpdateBulletCount(gameState.p2.bullets);
-            player2.UpdateGrenadeCount(gameState.p2.grenades);
-            player2.UpdateShieldCount(gameState.p2.num_shield);
-            player2.deathCounter.UpdatePlayerDeathCount(gameState.p2.num_deaths);
+            player2.UpdateBulletCount(p2.bullets);
+            player2.UpdateGrenadeCount(p2.grenades);
+            player2.UpdateShieldCount(p2.num_shield);
+            player2.deathCounter.UpdatePlayerDeathCount(p2.num_deaths);
         }
         
         // Display action (not necessary)
-        simulatorMessage.text = "" + gameState.p1.action;
+        simulatorMessage.text = "" + p1.action;
 
         // Perform actions only when there are no warning messages
-        if(gameState.p1.invalid == null)
-        {      if(gameState.p1.action == "grenade")
+        if(p1.invalid == null)
+        {      
+            if(p1.action == "grenade")
             {
                 if(player2.ReturnTargetQuery())
                 {
@@ -102,56 +105,56 @@ public class mqttStateController : MonoBehaviour
                     _eventSender.Publish();
                 }
             }
-            if(gameState.p1.action == "shoot") // Need to check if hit
+            if(p1.action == "shoot") // Need to check if hit
             {
-                if(gameState.p1.shot == true)
+                if(p1.shot == true)
                 {
                     player1.Bullet();
                     soundEffect.playBulletShootSound();
                     soundEffect.playHitSound();
                 }
-                else if(gameState.p1.shot == false)
+                else if(p1.shot == false)
                 {
                     soundEffect.playMissSound();
                 }         
             }
-            else if(gameState.p2.action == "shoot")
+            else if(p2.action == "shoot")
             {
-                if(gameState.p2.shot == true)
+                if(p2.shot == true)
                 {
                     player2.Bullet();
                     // Probably add player grunting sound
-                    if(gameState.p1.shield_health >= 0)
+                    if(p1.shield_health >= 0)
                     {
                         soundEffect.playsShieldCooldownSound();
                     }
                 }
-                else if(gameState.p2.shot == false)
+                else if(p2.shot == false)
                 {
                     //  Opponent has missed shooting you
                 }
             }
-            else if(gameState.p1.action == "shield")
+            else if(p1.action == "shield")
             {
                 player1.ActivateShield();
                 soundEffect.playShieldActivationSound();
-                shieldTimer.SetTime(gameState.p1.shield_time);
+                shieldTimer.SetTime(p1.shield_time);
             }
-            else if(gameState.p2.action == "shield")
+            else if(p2.action == "shield")
             {
                 player2.ActivateShield();
-                shieldTimer.SetTime(gameState.p2.shield_time);
+                shieldTimer.SetTime(p2.shield_time);
             }
-            else if(gameState.p1.action == "reload")
+            else if(p1.action == "reload")
             {
                 player1.ReloadBullets();
                 soundEffect.playReloadSound();
             }
-            else if(gameState.p2.action == "reload")
+            else if(p2.action == "reload")
             {
                 player2.ReloadBullets();
             }
-            else if(gameState.p1.action  == "logout" || gameState.p2.action == "logout")
+            else if(p1.action  == "logout" || p2.action == "logout")
             {
                 player1.Logout();
                 player2.Logout();
@@ -160,7 +163,7 @@ public class mqttStateController : MonoBehaviour
         else 
         {
             // Display Warning message
-            message.SetWarning(gameState.p1.invalid);
+            message.SetWarning(p1.invalid);
         }
     }
 }
